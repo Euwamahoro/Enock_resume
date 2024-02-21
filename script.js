515edb676878d44c98b9f96b7089151487215bce
Euwamahoro <e.uwamahoro@alustudent.com>
2024-02-22 01:16:31 +0200

    This is Enock

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     typeText();
 });
+ // Smooth scrolling script
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
